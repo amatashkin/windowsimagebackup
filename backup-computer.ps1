938541b84d9f5559023f67f98bdd5683541c9ba2
Alexey Matashkin <amatashkin@gmail.com>
2013-10-09 17:04:54 +0400

    Cleanup. Indent.

--- backup-computer.ps1
+++ backup-computer.ps1
@@ -11,27 +11,27 @@ $adminRole = [System.Security.Principal.WindowsBuiltInRole]::Administrator
  
 # Check to see if we are currently running "as Administrator"
 if ($myWindowsPrincipal.IsInRole($adminRole)) {
-   # We are running "as Administrator" - so change the title and background color to indicate this
-   $Host.UI.RawUI.WindowTitle = $myInvocation.MyCommand.Definition + "(Elevated)"
-   $Host.UI.RawUI.BackgroundColor = "DarkBlue"
-   clear-host
+    # We are running "as Administrator" - so change the title and background color to indicate this
+    $Host.UI.RawUI.WindowTitle = $myInvocation.MyCommand.Definition + "(Elevated)"
+    $Host.UI.RawUI.BackgroundColor = "DarkBlue"
+    clear-host
 }
 else {
-   # We are not running "as Administrator" - so relaunch as administrator
-   
-   # Create a new process object that starts PowerShell
-   $newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell";
-   
-   # Specify the current script path and name as a parameter
-   $newProcess.Arguments = $myInvocation.MyCommand.Definition;
-   
-   # Indicate that the process should be elevated
-   $newProcess.Verb = "runas";
-   
-   # Start the new process
-   [System.Diagnostics.Process]::Start($newProcess);
-   # Exit from the current, unelevated, process
-   exit
+    # We are not running "as Administrator" - so relaunch as administrator
+
+    # Create a new process object that starts PowerShell
+    $newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell";
+
+    # Specify the current script path and name as a parameter
+    $newProcess.Arguments = $myInvocation.MyCommand.Definition;
+
+    # Indicate that the process should be elevated
+    $newProcess.Verb = "runas";
+
+    # Start the new process
+    [System.Diagnostics.Process]::Start($newProcess);
+    # Exit from the current, unelevated, process
+    exit
 }
  
 # From this point running Backup in Elevated mode
